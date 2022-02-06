d4c03a2c281512e497e3619e1a1c9115f9b05904
venera111 <lysovayana@mail.ru>
2022-02-06 14:14:19 +0300

    fill struct

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: qestefan <qestefan@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 12:49:54 by qestefan          #+#    #+#             */
-/*   Updated: 2022/02/06 12:59:39 by qestefan         ###   ########.fr       */
+/*   Updated: 2022/02/06 14:12:37 by qestefan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ int	main(int argc, char **argv)
 	if (argc != 5 && argc != 6)
 		ft_perror(ERROR_ARGC);
 	memset(&data, 0, sizeof(data)); // заполняем нулями поля структуры
+	
 	return (0);
 }
