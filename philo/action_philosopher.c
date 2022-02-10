aa18baabb64dc2fe833c63f17dda569b36367106
Yana Lysova <lysovayana@Yanas-MacBook-Air.local>
2022-02-10 19:39:19 +0300

    up

--- philo/action_philosopher.c
+++ philo/action_philosopher.c
@@ -6,7 +6,7 @@
 /*   By: qestefan <qestefan@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 20:33:25 by qestefan          #+#    #+#             */
-/*   Updated: 2022/02/09 19:57:19 by qestefan         ###   ########.fr       */
+/*   Updated: 2022/02/10 19:38:49 by qestefan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	*action_philosopher(void *data)
 	philosopher = data;
 	if (philosopher->n % 2 == 0) //если четный философ
 		usleep(philosopher->data->time_to_eat * 1000); //устанавливаем в main структуре отрезок времени приема пищи в мс
-	
+
 }
